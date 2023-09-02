6eb5c7a3a9085b085c9e88a352a21f84217da8ed
Fran Vázquez <45006637+franvazquezporras@users.noreply.github.com>
2023-09-02 18:51:55 +0200

    Mute buttons control

---
+++ Curriculum/Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isMusicMuted = false;
+    public bool isSoundMuted = false;    
+}
